4d3d935566b92eb41eeb7962ad42744240c65a70
MuhammadH <Muhammadhauris@gmail.com>
2021-03-14 00:35:44 -0700

    movies code

--- Movies.js
+++ Movies.js
@@ -13,8 +13,8 @@ try {
 mongoose.set('useCreateIndex', true);
 
 //user schema
-var MovieSchemaB = new Schema({
-   title: {type: String, required: true, unique: true},
+var MovieSchemaC = new Schema({
+   title: {type: String, required: true, index: { unique: true }},
    year: {type: String, required: true},
    genre: {type: String, required: true},
    cast: [{
@@ -23,7 +23,7 @@ var MovieSchemaB = new Schema({
    }]
 });
 
-MovieSchemaB.pre('save', function(next) {
+MovieSchemaC.pre('save', function(next) {
    var movie = this;
 
    if (movie.cast.length < 3) {
@@ -36,4 +36,4 @@ MovieSchemaB.pre('save', function(next) {
 });
 
 //return the model to server
-module.exports = mongoose.model('Movie', MovieSchemaB);
+module.exports = mongoose.model('Movie', MovieSchemaC);
