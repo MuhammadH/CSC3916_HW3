c1f6f11a6e33468ea8e0ce2a4d846b2674ed1875
MuhammadH <Muhammadhauris@gmail.com>
2021-03-13 23:39:20 -0700

    second commit

---
+++ Movies.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected - movies"));
+}catch (error) {
+    console.log("could not connect - movies");
+}
+mongoose.set('useCreateIndex', true);
+
+//user schema
+var MovieSchema = new Schema({
+   title: {type: String, required: true},
+   year: {type: String, required: true},
+   genre: {type: String, required: true},
+   cast: [{
+       actor: {type: String, required: true},
+       character: {type: String, required: true}
+   }]
+});
+
+MovieSchema.pre('save', function(next) {
+   var movie = this;
+
+   if (movie.cast.length < 3) {
+       var err = new ValidationError(this);
+       err.errors.movie = new ValidatorError('need 3 or more actors in cast');
+       next(err);
+   } else {
+       next();
+   }
+});
+
+//return the model to server
+module.exports = mongoose.model('Movie', MovieSchema);
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ var authJwtController = require('./auth_jwt');
 var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var User = require('./Users');
+var Movie = require('./Movies');
 
 var app = express();
 app.use(cors());
@@ -84,6 +85,68 @@ router.post('/signin', function (req, res) {
         })
     })
 });
+
+router.route('/movies')
+    .post(function(req, res) {
+        var new_movie = new Movie();
+
+        new_movie.title = req.body.title;
+        new_movie.year = req.body.year;
+        new_movie.genre = req.body.genre;
+
+        let cast_length = req.body.cast.length;
+        for (let i = 0; i < cast_length; i++) {
+            new_movie.cast.push(JSON.parse(JSON.stringify(req.body.cast[i])));
+        }
+
+        user.save(function(err){
+            if (err) {
+                if (err.code == 11000)
+                    return res.json({ success: false, message: 'A movie with that name already exists.'});
+                else
+                    return res.json(err);
+            }
+
+            res.json({success: true, msg: 'Successfully created new movie.'})
+        });
+        }
+    )
+    /*
+    .get(function(req, res) {
+        console.log(req.body);
+        // set status code
+        res = res.status(200);
+        if (req.get('Content-Type')) {
+            res = res.type(req.get('Content-Type'));
+        }
+        var req_obj = getJSONObjectForMovieRequirement(req);
+        res.json({success: true, msg: 'get movies', headers: req_obj.headers, query: req.query, host: req_obj.key});
+    }
+    )
+    .put(authJwtController.isAuthenticated, function(req, res) {
+            console.log(req.body);
+            // set status code
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                res = res.type(req.get('Content-Type'));
+            }
+            var req_obj = getJSONObjectForMovieRequirement(req);
+            res.json({success: true, msg: 'put movies', headers: req_obj.headers, query: req.query, host: req_obj.key});
+        }
+    )
+    .delete(authController.isAuthenticated, function(req, res) {
+            console.log(req.body);
+            // set status code
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                res = res.type(req.get('Content-Type'));
+            }
+            var req_obj = getJSONObjectForMovieRequirement(req);
+            res.json({success: true, msg: 'delete movies', headers: req_obj.headers, query: req.query, host: req_obj.key});
+        }
+    )
+*/
+;
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
